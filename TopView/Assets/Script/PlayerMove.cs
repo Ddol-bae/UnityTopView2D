52a6db7a6af1c07d924df4a6ceca66763155318a
Ddol-bae <41590760+Ddol-bae@users.noreply.github.com>
2021-09-17 17:23:40 +0900

    TopView2D 2일차
    추가_
    캐릭터 애니메이션
    수정_
    캐릭터 이동이 안되던 것
    캐릭터가 화면 밖으로 밀려났던 것
    (BlockObject와 충돌오류)
    버그_
    캐릭터 WALK 애니메이션 LOOP이 안됨

--- TopView/Assets/Script/PlayerMove.cs
+++ TopView/Assets/Script/PlayerMove.cs
@@ -3,25 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
-{
+{   
+    public float Speed = 5;
+    Rigidbody2D rigid; 
     float h;
     float v;
-    Rigidbody2D rigid;
+    bool isHorizonMove;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
-    {
+    {   //move value
         h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("vertical");
+        v = Input.GetAxisRaw("Vertical");
+        
+        //check button down & up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        //check horizontal move
+        if (hDown || vUp)
+            isHorizonMove = true;
+        else if (vDown || hUp)
+            isHorizonMove = false;
+
+            //animation
+        anim.SetInteger("Haxis", (int)h);
+        anim.SetInteger("Vaxis", (int)v);
+
+        
     }
 
     void FixedUpdate()
-    {
-        rigid.velocity = new Vector2(h, v); 
+    {   
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v); ; 
+        rigid.velocity = moveVec * Speed; 
     }
 
 }
